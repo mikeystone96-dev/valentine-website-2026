b8eed73f7cc27c29d024c86b6c67e7a82950dcbe
mikeystone96-dev <mikey.stone96@yahoo.com>
2026-01-14 12:08:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const CONFIG = {
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['💜', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻' '']                       // Cute bear emojis
+        bears: ['🧸', '🐻' '🌹']                       // Cute bear emojis
     },
 
     // Questions and answers
